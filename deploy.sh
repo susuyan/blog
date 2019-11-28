9668b6a781112fca96b6e4e6eab13ff064df464c
susuyan <susuyan@163.com>
2019-11-28 18:08:14 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<susuyan>/<susuyan>.github.io.git master
+git push -f git@github.com:<susuyan>/<susuyan>.github.io.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
