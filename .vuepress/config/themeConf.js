ec091d4522b23c6ca7fd7ceae8b16f691ad76af3
susuyan <susuyan@163.com>
2019-11-29 12:16:54 +0800

    update

--- .vuepress/config/themeConf.js
+++ .vuepress/config/themeConf.js
@@ -8,17 +8,42 @@ module.exports = {
   lastUpdated: "更新于",
   sidebar,
   nav: [
-   //Swift
+   //iOS
    {
        text: "Swift",
        items: [
         {
-            text: "Swift",
+            text: "The Swift",
             link: ""
-   
-        }
-
+        },
+        {
+            text: "The iOS",
+            link: ""
+        },
+        {
+          text: "The Xcode",
+          link: ""
+        },
        ]
-   }
+   },
+   {
+    text: "数据结构与算法",
+    items: [
+     {
+         text: "数据结构",
+         link: ""
+     },
+     {
+         text: "算法",
+         link: ""
+     },
+     {
+       text: "设计模式",
+       link: ""
+     },
+    ]
+},
+
+
   ]
 };
