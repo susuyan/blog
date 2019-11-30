ba6dc33943f04b87c3603017d009a9ce27bf0c07
susuyan <susuyan@163.com>
2019-11-30 16:16:13 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -16,33 +16,33 @@ const {
       [ 
         '@vuepress/google-analytics', { 'ga': 'UA-63856296-2' }
       ],
-      // [
-      //   '@vuepress/pwa',
-      //   {
-      //     serviceWorker: true,
-      //     updatePopup: {
-      //       message: "发现页面有新内容",
-      //       buttonText: "刷新"
-      //     }
-      //   }
-      // ],
-      // [
-      //   'vuepress-plugin-comment',
-      //   {
-      //     choosen: 'gitalk', 
-      //     options: {
-      //       clientID: '7160032e9a31eb9e2259',
-      //       clientSecret: process.env.clientSecret || 'eb34433cb92d133b0c912e4c9d1dfed7c82bc47f',
-      //       repo: 'blog',
-      //       owner: 'susuyan',
-      //       admin: ['susuyan'],
-      //       id: '<%- frontmatter.commentid || frontmatter.permalink %>',      // Ensure uniqueness and length less than 50
-      //       distractionFreeMode: false,  // Facebook-like distraction free mode
-      //       labels: ['Gitalk', 'Comment'],
-      //       title: '「评论」<%- frontmatter.title %>',
-      //       body: '<%- frontmatter.title %>：<%- window.location.origin %><%- frontmatter.to.path || window.location.pathname %>'
-      //     }
-      //   }
-      // ]
+      [
+        '@vuepress/pwa',
+        {
+          serviceWorker: true,
+          updatePopup: {
+            message: "发现页面有新内容",
+            buttonText: "刷新"
+          }
+        }
+      ],
+      [
+        'vuepress-plugin-comment',
+        {
+          choosen: 'gitalk', 
+          options: {
+            clientID: '7160032e9a31eb9e2259',
+            clientSecret: process.env.clientSecret || 'eb34433cb92d133b0c912e4c9d1dfed7c82bc47f',
+            repo: 'blog',
+            owner: 'susuyan',
+            admin: ['susuyan'],
+            id: '<%- frontmatter.commentid || frontmatter.permalink %>',      // Ensure uniqueness and length less than 50
+            distractionFreeMode: false,  // Facebook-like distraction free mode
+            labels: ['Gitalk', 'Comment'],
+            title: '「评论」<%- frontmatter.title %>',
+            body: '<%- frontmatter.title %>：<%- window.location.origin %><%- frontmatter.to.path || window.location.pathname %>'
+          }
+        }
+      ]
     ]
   }
