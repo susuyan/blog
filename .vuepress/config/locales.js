3ffd4670853887152948707cec063784d46e93ae
susuyan <susuyan@163.com>
2019-11-30 14:48:26 +0800

    update

--- .vuepress/config/locales.js
+++ .vuepress/config/locales.js
@@ -2,7 +2,8 @@ module.exports = {
     "/": {
       lang: "zh-CN",
       title: "苏苏炎博客",
-      description: "专注iOS 和 Swift 的博客."
+      description: "专注iOS 和 Swift 的博客.",
+      base: '/blog/'
     }
   };
   
